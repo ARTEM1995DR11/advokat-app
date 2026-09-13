b4b99f9154d6cabb4236830419229ed22104ef25
Адвокат <korotkov.artem11@mail.ru>
2026-09-13 18:22:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE = 'advokat-iphone-offline-v40-stable-5093';
+const CACHE = 'advokat-iphone-offline-v40-stable-5094';
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=5093',
+  './styles.css?v=5094',
   './nav-panel-light.png',
-  './nav-active-base.png?v=5093',
-  './nav-active-today.png?v=5093',
-  './nav-active-tasks.png?v=5093',
-  './nav-active-cases.png?v=5093',
-  './nav-active-calendar.png?v=5093',
-  './nav-active-more.png?v=5093',
-  './app.js?v=5093',
-  './manifest.webmanifest?v=5093',
+  './nav-active-base.png?v=5094',
+  './nav-active-today.png?v=5094',
+  './nav-active-tasks.png?v=5094',
+  './nav-active-cases.png?v=5094',
+  './nav-active-calendar.png?v=5094',
+  './nav-active-more.png?v=5094',
+  './app.js?v=5094',
+  './manifest.webmanifest?v=5094',
   './premium-icon-180.png',
   './premium-icon-192.png',
   './premium-icon-512.png',
@@ -23,9 +23,9 @@ const SHELL = [
   './bg-cal-marble-calendar.png',
   './bg-more-marble-office.png',
   './fab-plus-square-premium.png',
-  './header-bell-premium.png?v=5093',
-  './header-search-premium.png?v=5093',
-  './quick-sheet-marble-approved.png?v=5093',
+  './header-bell-premium.png?v=5094',
+  './header-search-premium.png?v=5094',
+  './quick-sheet-marble-approved.png?v=5094',
   './VERSION.txt'
 ];
 
