6993ee2abe4840df142aab41bec8ffda781019ef
Адвокат <korotkov.artem11@mail.ru>
2026-09-20 13:26:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'advokat-iphone-offline-v121-premium-5341';
+const CACHE = 'advokat-iphone-offline-v121-premium-5343';
 
 const CORE = [
   './',
   './index.html',
-  './styles.css?v=5341',
-  './app.js?v=5341',
-  './manifest.webmanifest?v=5341',
+  './styles.css?v=5343',
+  './app.js?v=5343',
+  './manifest.webmanifest?v=5343',
   './VERSION.txt',
   './premium-icon-180.png',
   './premium-icon-192.png',
