c425da6af420565132ddafb6ffbdb59deb0208d8
Адвокат <korotkov.artem11@mail.ru>
2026-09-21 22:31:31 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'advokat-iphone-offline-v121-premium-5406';
+const CACHE = 'advokat-iphone-offline-v121-premium-5407';
 
 const CORE = [
   './',
   './index.html',
-  './styles.css?v=5406',
-  './app.js?v=5406',
-  './manifest.webmanifest?v=5406',
+  './styles.css?v=5407',
+  './app.js?v=5407',
+  './manifest.webmanifest?v=5407',
   './VERSION.txt',
   './premium-icon-180.png',
   './premium-icon-192.png',
@@ -23,8 +23,8 @@ const CORE = [
   './columns-light.png?v=5249',
   './header-bell-premium.png?v=5249',
   './header-search-premium.png?v=5249',
-  './header-filter-premium.png?v=5406',
-  './bg-new-matter-approved.webp?v=5406',
+  './header-filter-premium.png?v=5407',
+  './bg-new-matter-approved.webp?v=5407',
   './global-search-head-motif-v173.png?v=5249',
   './fab-plus-square-premium.png?v=5249',
   './nav-panel-light.png',
