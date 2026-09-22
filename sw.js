7656c493a7e655d7f77bc49f2e0483fc550d2c2d
Адвокат <korotkov.artem11@mail.ru>
2026-09-22 16:43:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'advokat-iphone-offline-v121-premium-5457';
+const CACHE = 'advokat-iphone-offline-v121-premium-5459';
 
 const CORE = [
   './',
   './index.html',
-  './styles.css?v=5457',
-  './app.js?v=5457',
-  './manifest.webmanifest?v=5457',
+  './styles.css?v=5459',
+  './app.js?v=5459',
+  './manifest.webmanifest?v=5459',
   './VERSION.txt',
   './premium-icon-180.png',
   './premium-icon-192.png',
@@ -23,11 +23,11 @@ const CORE = [
   './columns-light.png?v=5249',
   './header-bell-premium.png?v=5249',
   './header-search-premium.png?v=5249',
-  './header-filter-premium.png?v=5457',
-  './bg-new-matter-approved.webp?v=5457',
-  './nm-folder-mockup.png?v=5457',
-  './nm-scale-mockup.png?v=5457',
-  './nm-doc-mockup.png?v=5457',
+  './header-filter-premium.png?v=5459',
+  './bg-new-matter-approved.webp?v=5459',
+  './nm-folder-mockup.png?v=5459',
+  './nm-scale-mockup.png?v=5459',
+  './nm-doc-mockup.png?v=5459',
   './global-search-head-motif-v173.png?v=5249',
   './fab-plus-square-premium.png?v=5249',
   './nav-panel-light.png',
