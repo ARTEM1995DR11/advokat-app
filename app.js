08c74001ca84b077b1b32f7912ba5e8550f2450b
Адвокат <korotkov.artem11@mail.ru>
2026-09-22 23:24:29 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
    ===================================================================== */
 'use strict';
 
-var APP_VERSION='5.0.474';
-var APP_BUILD='5474';
+var APP_VERSION='5.0.475';
+var APP_BUILD='5475';
 
 /* ------------------------- state + encrypted local storage ------------------------- */
 var KEY = 'advokat_pro_v1'; // legacy localStorage key (migration only)
